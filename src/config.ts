30a5d7a14d7c3c2194670f7fb9f2879a3170be65
Солодов Евгений Александрович <evgenii.kgg05@gmail.com>
2020-06-17 18:19:23 +0700

    fix2

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,5 @@
 export const isProduction = process.env.NODE_ENV === 'production';
 
-<<<<<<< HEAD
 //TODO получить или сделать сертификат
 export const baseUrl = 'https://api.experts-gift.eltc.ru/api';
-=======
-//TODO получить или сделать сертификат 
-export const baseUrl = "https://api.experts-gift.eltc.ru/api";
->>>>>>> fix
 
